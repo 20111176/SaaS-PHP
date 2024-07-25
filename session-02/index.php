8d04ffba18988571b837e11a2675a126d3f0503e
Cedric Ko <yacmov@gmail.com>
2024-07-26 00:17:54 +0800

    feat: session 02 started

---
+++ session-02/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Block comments for php
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+/*
+ * Multi line comment
+ * ta-da!
+ */
+
+
+?>
+</body>
+</html>
+
+<?php
+// End of file
+// one line comment
